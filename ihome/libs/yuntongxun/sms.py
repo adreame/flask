58c177ba265018b4eb8c0ffcca705fd775530ed8
adreame <2745517648@qq.com>
2020-07-06 07:56:27 +0800

    first commit

---
+++ ihome/libs/yuntongxun/sms.py
@@ -0,0 +1,57 @@
+from SmsSDK import SmsSDK
+
+accId = '容联云通讯分配的主账号ID'
+accToken = '容联云通讯分配的主账号TOKEN'
+appId = '容联云通讯分配的应用ID'
+
+
+class CCP(object):
+    """自己封装的发送短信的辅助类"""
+    # 用来保存对象的类属性
+    instance = None
+
+    def __new__(cls):
+        # 判断CCP类有没有已经创建好的对象，如果没有，创建一个对象，并且保存
+        # 如果有，则将保存的对象直接返回
+        if cls.instance is None:
+            obj = super(CCP, cls).__new__(cls)
+
+            # 初始化REST SDK
+            obj.sdk = SmsSDK(accId, accToken, appId)
+
+            cls.instance = obj
+
+        return cls.instance
+
+    def send_template_sms(self, tid, mobile, datas):
+        """"""
+        # tid = '容联云通讯创建的模板'
+        # mobile = '手机号1,手机号2'
+        # datas = ('变量1', '变量2')
+        # resp = sdk.sendMessage(tid, mobile, datas)
+
+        result = self.sdk.sendMessage(tid, mobile, datas)
+        # for k, v in result.iteritems():
+        #
+        #     if k == 'templateSMS':
+        #         for k, s in v.iteritems():
+        #             print '%s:%s' % (k, s)
+        #     else:
+        #         print '%s:%s' % (k, v)
+        # smsMessageSid:ff75e0f84f05445ba08efdd0787ad7d0
+        # dateCreated:20171125124726
+        # statusCode:000000
+        status_code = eval(result).get("statusCode")
+        if status_code == "000000":
+            # 表示发送短信成功
+            return 0
+        else:
+            # 发送失败
+            return -1
+
+
+if __name__ == '__main__':
+    ccp = CCP()
+    ret = ccp.send_template_sms(1, "18339165865", ("1234", "5"))
+    print(ret)
+
