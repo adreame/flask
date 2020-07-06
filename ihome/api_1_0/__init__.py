0db861082c487a46e2e2a99bea2d80034f2fb96a
adreame <56779352+adreame@users.noreply.github.com>
2020-07-06 08:48:21 +0800

    Update __init__.py

--- ihome/api_1_0/__init__.py
+++ ihome/api_1_0/__init__.py
@@ -8,4 +8,4 @@ api = Blueprint("api_1_0", __name__)
 
 
 # 导入蓝图的视图
-from . import demo, verify_code, passport, profile, houses, orders, pay
+from . import verify_code, passport, profile, houses, orders, pay
